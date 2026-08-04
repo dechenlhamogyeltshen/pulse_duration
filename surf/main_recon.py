db71e78db1bb4bfae6d87725f85e8fa9b3c7abda
Dechen Lhamo Gyeltshen <dven@Dechens-MacBook-Pro.local>
2026-08-04 14:45:49 +0100

    Main code added

--- surf/main_recon.py
+++ surf/main_recon.py
@@ -78,7 +78,7 @@ project_dirs = surf._setup_dirs_()
 crpath = os.path.join(project_dirs['input'],'(I)CMEs.csv')
 
 # Load the CSV file into a DataFrame
-crlist = pd.read_csv(crpath,nrows=2)
+crlist = pd.read_csv(crpath)
 
 # Convert date columns to datetime objects using datetime library
 date_columns = ['Time_21.5', 'Disturbance_Time']
@@ -182,7 +182,7 @@ arrival_speed.append(['Velocity'] + list(crlist['CME_V']))
 transit_time.append(['Observed'] + list(crlist['tt_21']))
 arrival_speed.append(['Observed'] + list(crlist['V_max']))
 
-durations = np.arange (10.0,11.0,0.5)#np.arange(1.0, 30.5, 0.5)  # CME durations in hours
+durations = np.arange(1.0, 30.5, 0.1)  # CME durations in hours
 
 # Pre-initialize rows
 sph_tt_row = ['Spheroidal']
